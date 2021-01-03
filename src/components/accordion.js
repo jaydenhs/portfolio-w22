037c8fe0957063cc11f95fd5310b3f46e387ddf2
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2021-01-03 13:40:05 -0500

    create accordion component with default HTML section

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,32 +1,11 @@
 import React from 'react';
 
-const Accordion = () => {
+const Accordion = ({ title, children }) => {
   return (
     <section>
-      <details open>
-        <summary>First Section</summary>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </details>
-
       <details>
-        <summary>Second Section</summary>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <summary className="mb-2">{title}</summary>
+        <p>{children}</p>
       </details>
     </section>
   );
