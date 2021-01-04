9c13f302720828d276ad05c68c714a92a5079e39
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2021-01-03 22:15:21 -0500

    style higher-fidelity timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,44 +1,69 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
 
-const Timeline = ({ start, markers }) => {
+const Timeline = ({ markers }) => {
+  const last_elem = markers.length - 1;
   return (
-    <div className="my-4 mx-24">
-      <TimelineLine />
-      <TimelineItems>
-        {markers.map((node, index) => {
-          return (
-            <TimelineItem key={index}>
-              <TimelineContent>{node}</TimelineContent>
-            </TimelineItem>
-          );
-        })}
-      </TimelineItems>
+    <div className="mt-8 mb-4">
+      <div className="flex">
+        <EndText className="text-right pr-5">{markers[0]}</EndText>
+        <div className="w-full">
+          <TimelineLine />
+          <TimelineItems>
+            {markers.map((node, index) => {
+              let first_or_last = index === 0 || index === last_elem;
+              return (
+                <TimelineItem key={index} first_or_last={first_or_last}>
+                  {!first_or_last && <TimelineContent>{node}</TimelineContent>}
+                </TimelineItem>
+              );
+            })}
+          </TimelineItems>
+        </div>
+        <EndText className="pl-10">{markers[last_elem]}</EndText>
+      </div>
     </div>
   );
 };
+
+const EndText = styled.p`
+  transform: translateY(calc(-20%));
+  ${tw`font-bold`}
+`;
 
 const TimelineLine = styled.div`
   ${tw`h-1 bg-gray-200 w-full rounded-md`}
 `;
 
 const TimelineItems = styled.div`
-  ${tw`-m-3 mb-0 flex justify-between`}
+  ${tw`-mt-3 flex justify-between w-full`}
 `;
 
-const TimelineItem = styled.div`
-  ${tw`w-5`}
-  &:before {
-    content: '';
-    ${tw`w-5 h-5 block rounded-full bg-primary`}
-  }
-`;
+const TimelineItem = styled.div(
+  ({ first_or_last }) => css`
+    ${first_or_last ? tw`w-0` : tw`w-40`}
+    transform: translateX(calc(50%));
+    &:before {
+      content: '';
+      ${tw`w-5 h-5 block rounded-full bg-primary`}
+    }
+  `
+);
 
 const TimelineContent = styled.div`
   transform: translateX(calc(-50% + 10px));
   background-color: rgba(var(--color-primary), 0.2);
-  ${tw`mt-3 rounded px-2 py-3 w-40 text-center`}
+  min-width: 160px;
+  ${tw`mt-3 rounded px-3 py-3 text-center`}
+
+  &:before {
+    content: '';
+    background-color: rgba(var(--color-primary), 0.2);
+    transform: rotate(-45deg) translateX(-50%);
+    margin-left: -0.11rem;
+    ${tw`w-2.5 h-2.5 block absolute -top-2 left-1/2`}
+  }
 `;
 
 export default Timeline;
