78e7cdea174542d8cc04e1d89946f650ab470896
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2021-01-13 22:09:42 -0500

    finish creating room!

--- src/components/room-object.js
+++ src/components/room-object.js
@@ -4,28 +4,52 @@ import Img from '@utils/local-img';
 import styled, { keyframes } from 'styled-components';
 import tw from 'twin.macro';
 
-const RoomObject = ({ children, src, tooltip, width, left, top }) => {
+const RoomObject = ({
+  children,
+  src,
+  tooltip,
+  width,
+  left,
+  top,
+  className,
+}) => {
   let place = top > 50 ? 'top' : 'bottom';
 
   //generate unique key for tooltip association
   var crypto = require('crypto');
   var id = crypto.randomBytes(20).toString('hex');
 
+  // convert all illustration-specific props to arrays (or else we can't map over strings)
+  // could simply wrap all inputs in [], but that's inefficient for inputs
+  let src_array = Array.isArray(src) ? src : [src];
+  let width_array = Array.isArray(width) ? width : [width];
+  let left_array = Array.isArray(left) ? left : [left];
+  let top_array = Array.isArray(top) ? top : [top];
+  let className_array = Array.isArray(className) ? className : [className];
+
   return (
-    <>
-      <div
-        data-tip={id}
-        data-for={id}
-        className="absolute h-auto"
-        style={{
-          width: `${width}%`,
-          left: `${left}%`,
-          top: `${top}%`,
-        }}
-      >
-        {tooltip && <Pointer />}
-        <Img src={src} />
-      </div>
+    <Wrapper>
+      {src_array.map((value, i) => {
+        console.log({ value });
+        return (
+          <div
+            key={i}
+            data-tip={id}
+            data-for={id}
+            className={`absolute h-auto ${className_array[i]}`}
+            style={{
+              width: `${width_array[i]}%`,
+              left: `${left_array[i]}%`,
+              top: `${top_array[i]}%`,
+            }}
+          >
+            {/* only render glowing pointer once for double tooltips */}
+            {tooltip && i === 0 && <Pointer />}
+            <Img src={value} />
+          </div>
+        );
+      })}
+
       {tooltip && (
         <Tooltip
           id={id}
@@ -37,9 +61,11 @@ const RoomObject = ({ children, src, tooltip, width, left, top }) => {
           {children}
         </Tooltip>
       )}
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.span``;
 
 const Tooltip = styled(ReactTooltip)`
   border: 5px solid var(--primaryLight) !important;
@@ -63,7 +89,7 @@ const glowing = keyframes`
 const Pointer = styled.span`
   //center pointer
   ${tw`absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2`}
-  ${tw`bg-white border-2 border-solid w-4 h-4 rounded-full z-10`}
+  ${tw`bg-white border-2 border-solid w-4 h-4 rounded-full z-50`}
 
   border-color: #4895ea;
   animation: ${glowing} 2.5s infinite;
--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ const About = () => {
         about me!
       </p>
       <div className="relative">
-        <Img src={'walls.png'} />
+        <Img src={'walls.svg'} />
         <RoomObject
           src={'diploma.svg'}
           svg={true}
@@ -29,10 +29,8 @@ const About = () => {
           of 2024 🔧 - ninety-six brilliant, innovative students who inspire me
           to do my best everyday! 🎉
         </RoomObject>
-
         <RoomObject src="bed.svg" width={36.38} left={27.12} top={27.82} />
         <RoomObject src="shelf.svg" width={12.59} left={28.64} top={19.38} />
-
         <RoomObject
           src="camera.svg"
           tooltip={true}
@@ -45,7 +43,6 @@ const About = () => {
           photography has always been one of my favourite methods of artistic
           expression! 📷
         </RoomObject>
-
         <RoomObject
           src="window.svg"
           tooltip={true}
@@ -58,10 +55,121 @@ const About = () => {
           I've tried to apply this same curiosity to explore myself, my work,
           and the world around me! 🌎
         </RoomObject>
+        <RoomObject src="lamp.svg" width={6.087} left={75.37} top={36.98} />
 
-        <RoomObject src="lamp.svg" width={6.087} left={80.84} top={38.5} />
-        {/* <RoomObject src="shelf_side.svg" width={6.087} left={80.84} top={38.5} /> */}
+        <RoomObject
+          src="shelf_side.svg"
+          width={4.472}
+          left={80.84}
+          top={32.5}
+        />
+        <RoomObject
+          src="shelf_shadow.svg"
+          width={9.83}
+          left={81.49}
+          top={63.61}
+        />
+        <RoomObject
+          src="shelf_level.svg"
+          width={12.63}
+          left={81.96}
+          top={59.79}
+        />
+        <RoomObject
+          src="chest.svg"
+          tooltip={true}
+          width={7.836}
+          left={82.35}
+          top={56.67}
+        >
+          <Tooltip_Img src="chest_tooltip.jpg" />
+          Whether it be planning a checkmate in chess ♟️, crafting the perfect
+          deck in Dominion ⚔️, or monopolizing the sheep trade in Catan 🐑, one
+          of my favourite ways to have fun is through board games!
+        </RoomObject>
+        <RoomObject
+          src="shelf_level.svg"
+          width={12.63}
+          left={81.96}
+          top={51.18}
+        />
+        <RoomObject
+          src="books.svg"
+          tooltip={true}
+          width={4.31}
+          left={82.81}
+          top={47.29}
+        >
+          <Tooltip_Img src="books_tooltip.jpg" />
+          Luke 6:31 is my favourite quote! 📖 I constantly strive to devote the
+          empathy, kindness, and care highlighted within this passage not only
+          to my design work, but my lifestyle as well. 💙
+        </RoomObject>
+        <RoomObject
+          src="shelf_level.svg"
+          width={12.63}
+          left={81.96}
+          top={42.56}
+        />
+        <RoomObject
+          src="trophy.svg"
+          tooltip={true}
+          width={4.06}
+          left={83.19}
+          top={38.99}
+        >
+          <Tooltip_Img src="trophy_tooltip.jpg" />I always try my best to excel
+          in everything that I do!
+          <ul>
+            <li>
+              <b>Best Pitch</b> - StarterHacks 2020 💬
+            </li>
+            <li>
+              <b>First Class Honours</b> - Grade 10 Piano Practical Exam 🎹
+            </li>
+            <li>
+              <b>Honourable Mention</b> - Jostens Canada Photo Contest 📷
+            </li>
+          </ul>
+        </RoomObject>
+        <RoomObject
+          src="shelf_level.svg"
+          width={12.63}
+          left={81.96}
+          top={33.94}
+        />
+        <RoomObject
+          src="shelf_side.svg"
+          width={4.472}
+          left={91.29}
+          top={39.23}
+        />
 
+        <RoomObject
+          src="keyboard.svg"
+          tooltip={true}
+          width={22.77}
+          left={59.88}
+          top={61.47}
+        >
+          <Tooltip_Img src="keyboard_tooltip.jpg" />I absolutely love music! You
+          can often find me jamming out on the piano 🎹, improvising on the
+          saxophone 🎷, or performing acapella on-campus! 🎤
+        </RoomObject>
+
+        <RoomObject
+          src={['jayden_body.svg', 'jayden_arms.svg']}
+          tooltip={true}
+          width={[11.72, 9.63]}
+          left={[26.88, 26.54]}
+          top={[45.903, 52.1]}
+          className={['', 'z-10']}
+        >
+          <Tooltip_Img src="keyboard_tooltip.jpg" />I absolutely love music! You
+          can often find me jamming out on the piano 🎹, improvising on the
+          saxophone 🎷, or performing acapella on-campus! 🎤
+        </RoomObject>
+        <RoomObject src="table.svg" width={22.36} left={17.067} top={54.91} />
         <RoomObject
           src="sushi.svg"
           tooltip={true}
@@ -73,6 +181,26 @@ const About = () => {
           Some of my most treasured memories revolve around food! 😋 So let me
           know if you know any good restaurants, especially for sushi! 🍣
         </RoomObject>
+        <RoomObject
+          src={['laptop_bottom.svg', 'laptop_top.svg']}
+          tooltip={true}
+          width={[8.95, 6.32]}
+          left={[24.11, 22.81]}
+          top={[57.08, 55.24]}
+          className={['', 'z-20']}
+        >
+          <Tooltip_Img src="laptop_tooltip.jpg" />I believe that designers
+          should understand the technical functionality of the products they are
+          designing for. 💭 From C++ to C#, ReactJS to GraphQL, I am equipped to
+          adapt and optimize my designs! ⚙️
+        </RoomObject>
+
+        <RoomObject
+          src="large_plant.svg"
+          width={17.84}
+          left={35.89}
+          top={60.726}
+        />
       </div>
       {/* <Img src={'bed.png'} /> */}
     </Layout>
