e56d87445e14936e9a43874b4873bb1359271a6f
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2021-01-19 12:51:24 -0500

    finish v1 of TD design system case study!

--- src/components/figma-embed.js
+++ src/components/figma-embed.js
@@ -3,11 +3,11 @@ import React from 'react';
 const FigmaEmbed = ({ title, src }) => {
   return (
     <iframe
+      allowfullscreen
       title={title}
       width="100%"
       className="h-screen py-4"
       src={src}
-      allowfullscreen
     ></iframe>
   );
 };
