f665b9d1b2ea0aae360f1d588e89da852064b86c
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2021-01-19 13:00:19 -0500

    flip and tone down brightness of waves

--- src/components/main-project-preview.js
+++ src/components/main-project-preview.js
@@ -40,6 +40,11 @@ const ProjectCard = styled(GatsbyLink)`
     ${tw`-translate-y-1.5`}
     box-shadow: 0px 12px 17px 0px var(--boxShadow2);
   }
+
+  &:nth-child(even) .wave {
+    //flip wave for even project card
+    ${tw`transform -scale-x-1`}
+  }
 `;
 
 const Details = styled.div`
@@ -52,13 +57,13 @@ const Wave = () => (
     viewBox="0 0 1110 157"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    className="absolute bottom-0 left-0 w-full rounded-b-xl"
+    className="absolute bottom-0 left-0 w-full rounded-b-xl wave"
   >
     <path
       fill-rule="evenodd"
       clip-rule="evenodd"
       d="M1110 57.1148V445.002C1110 453.838 1102.84 461.002 1094 461.002H16C7.16346 461.002 0 453.838 0 445.002V63.5932C87.3285 44.6899 191.891 42.6994 296.124 40.7152C364.629 39.4111 432.993 38.1097 496.228 32.0114C533.326 28.4336 570.516 23.9378 607.591 19.456C770.027 -0.180222 930.246 -19.5483 1070.75 39.7377C1083.9 45.287 1097 51.0827 1110 57.1148Z"
-      fill="var(--primary)"
+      fill="hsla(var(--primaryHS) var(--normal), 85%)"
     />
   </svg>
 );
