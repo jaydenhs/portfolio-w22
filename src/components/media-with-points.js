aad77afa6ee1c14de61f6ea6acf4d9985cc3310f
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2021-06-06 15:06:30 -0400

    implement inclusivity OPC graphic

--- src/components/media-with-points.js
+++ src/components/media-with-points.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import tw from 'twin.macro';
 import Image from '@utils/local-img';
 
-const MediaWithPoints = ({ media, children }) => {
+const MediaWithPoints = ({
+  media,
+  children,
+  mediaAlignment,
+  imageClassName,
+}) => {
+  mediaAlignment = mediaAlignment ?? 'left';
+
   return (
-    <div
-      className="flex mx-auto items-center even:flex-row-reverse"
-      style={{ maxWidth: 960 }}
-    >
-      <Image src={media} className="max-h-screen max-w-xs" />
+    <Wrapper mediaAlignment={mediaAlignment} style={{ maxWidth: 960 }}>
+      <div className={`w-1/2 transform ${imageClassName}`}>
+        <Image src={media} className="max-h-screen" />
+      </div>
       {/* can't use space-x-# class because the flex direction reverses (margin on wrong side) */}
       <div className="mx-5"></div>
-      <div className="flex-grow space-y-2">{children}</div>
-    </div>
+      <div className="w-1/2 space-y-2">{children}</div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div(
+  ({ mediaAlignment }) => css`
+    ${tw`flex mx-auto w-full items-center`}
+    ${mediaAlignment === 'left' ? tw`flex-row` : tw`flex-row-reverse`}
+
+    max-width: 960px;
+  `
+);
 
 export default MediaWithPoints;
