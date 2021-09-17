4151614815e71c8db2ff5e09271e73f647dd0c6b
JaydenHsiao <j3hsiao@edu.uwaterloo.ca>
2021-09-17 11:02:24 -0400

    add password protection

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Jayden Hsiao's Portfolio`,
@@ -9,7 +13,13 @@ module.exports = {
       linkedin: `https://www.linkedin.com/in/jaydenh`,
     },
   },
-  flags: { PRESERVE_WEBPACK_CACHE: true },
+  flags: {
+    PRESERVE_WEBPACK_CACHE: true,
+    FAST_DEV: true,
+    QUERY_ON_DEMAND: true,
+    LAZY_IMAGES: true,
+    FAST_REFRESH: true,
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     //mdx post generation
@@ -48,6 +58,13 @@ module.exports = {
           '@undraw': 'static/undraw',
         },
         extensions: ['js'],
+      },
+    },
+    {
+      resolve: '@mkitio/gatsby-theme-password-protect',
+      options: {
+        password: process.env.PASSWORD, // delete or `undefined` to disable password protection
+        pagePaths: ['/structionsite'],
       },
     },
     // `gatsby-plugin-offline`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -37,3 +37,11 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     });
   });
 };
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    node: {
+      fs: 'empty',
+    },
+  });
+};
